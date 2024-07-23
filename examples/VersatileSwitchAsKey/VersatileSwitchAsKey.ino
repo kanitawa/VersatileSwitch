737add6d4d9f610857cff8e75e94000e0a606a39
kawamoto <kawamoto@saga-ls.jp>
2024-07-23 17:12:38 +0900

    make examples/AsKey and AsButton.

--- examples/VersatileSwitchAsKey/VersatileSwitchAsKey.ino
+++ examples/VersatileSwitchAsKey/VersatileSwitchAsKey.ino
@@ -1,18 +1,34 @@
 /*
-    VersatileSwaitchAsKey.ino
+    VersatileSwitchAsKey.ino
     Example code for using switch as a key.
 */
 
-#include <VersatileSwitch>
+#include <VersatileSwitch.h>
 
 #define PIN_SW 7
 
 VersatileSwitch sw(PIN_SW, INPUT_PULLUP);
 
 void setup() {
+  Serial.begin(9600);
+  while (!Serial) { }
+  Serial.println("*** demo VersatileSwitchAsKey ***");
 
+  // attach callback functions using as a key
+  sw.attachCallback_Pressed(on_switch_pressed);
+  sw.attachCallback_Repeated(on_switch_repeated);
 }
 
 void loop() {
-    
+  // polling switches
+  sw.poll();
+}
+
+// callback functions
+void on_switch_pressed() {
+  Serial.println("Pressed.");
+}
+
+void on_switch_repeated() {
+  Serial.println("Repeated.");
 }
